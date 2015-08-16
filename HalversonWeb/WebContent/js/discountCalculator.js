ceff8485e242d9a024c35faf9f775dd78840afe8
Dan Halverson <daniel.m.halverson@gmail.com>
2015-08-15 23:01:53 -0500

    converting to minified js

--- HalversonWeb/WebContent/js/discountCalculator.js
+++ HalversonWeb/WebContent/js/discountCalculator.js
@@ -1,26 +1,13 @@
 'use strict';
 
-$('#reset').click(function () {
-    calculator.clear();
-});
-
-$('#calculate').click(
-    function () {
-        calculator.clear();
-        calculator.validate($('#discount1').val(), $('#discount2').val(),
-            $('#labelPrice').val());
-        if (!calculator.isError()) {
-            calculator.calculate();
-        }
-    });
-
-var calculator = (function () {
+var calculatorService = (function () {
 
     var discount1 = 0;
     var discount2 = 0;
     var labelPrice = 0.00;
     var error = false;
     var finalPrice = 0.00;
+    var message = [];
 
     function setFirstDiscount(val) {
         discount1 = val;
@@ -42,20 +29,8 @@ var calculator = (function () {
         return finalPrice;
     }
 
-    function danger(message) {
-        $('#alert_placeholder')
-            .append(
-            '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button><strong>Error! </strong>' + message + '</div>');
-    }
-
-    function success(message) {
-        $('#alert_placeholder')
-            .html(
-            '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>' + message + '</div>');
-    }
-
-    function clear() {
-        $('#alert_placeholder').html('');
+    function getMessage() {
+        return message;
     }
 
     function calculate() {
@@ -67,25 +42,26 @@ var calculator = (function () {
             newPrice = firstCalc - (firstCalc * (discount2 / 100));
         }
         finalPrice = newPrice.toFixed(2);
-        success('Your final price is $' + finalPrice + ' plus tax');
+        message.push('Your final price is $' + finalPrice + ' plus tax');
     }
 
     function validate(firstDiscount, secondDiscount, labelPriceVal) {
+        message = [];
         error = false;
         if (labelPriceVal.length === 0 || isNaN(labelPriceVal)) {
-            danger('Label price is required and must be a number');
+            message.push("Label price is required and must be a number");
             error = true;
         } else {
             labelPrice = labelPriceVal;
         }
         if (firstDiscount.length === 0 || isNaN(firstDiscount)) {
-            danger('Discount #1 is required and must be a number');
+            message.push("Discount #1 is required and must be a number");
             error = true;
         } else {
             discount1 = firstDiscount;
         }
         if (isNaN(secondDiscount)) {
-            danger('Discount #2 must be a number, if provided');
+            message.push("Discount #2 must be a number, if provided");
             error = true;
         } else {
             discount2 = secondDiscount;
@@ -100,6 +76,37 @@ var calculator = (function () {
         setSecondDiscount: setSecondDiscount,
         setLabelPrice: setLabelPrice,
         getFinalPrice: getFinalPrice,
-        clear: clear
+        getMessage: getMessage
     };
 })();
+
+var ang_discountCalculator = angular.module('ang_discountCalculator', []);
+ang_discountCalculator.controller('discountCalculatorController', ['$scope', function ($scope) {
+    init();
+
+    $scope.ang_calculate = function () {
+        calculatorService.validate($scope.discount1, $scope.discount2,
+            $scope.labelPrice);
+        if (!calculatorService.isError()) {
+            calculatorService.calculate();
+            $scope.calculationMessage = calculatorService.getMessage();
+            $scope.successShow = true;
+        } else {
+            $scope.calculationMessage = calculatorService.getMessage();
+            $scope.errorShow = true;
+        }
+    };
+
+    $scope.ang_clear = function () {
+        init();
+    };
+
+    function init() {
+        $scope.errorShow = false;
+        $scope.successShow = false;
+        $scope.discount1 = "";
+        $scope.discount2 = "";
+        $scope.labelPrice = "";
+        $scope.calculationMessage = [];
+    }
+}]);
