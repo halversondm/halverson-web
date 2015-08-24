6269592597e82b71c0d8922a356531e80d6e51f0
Dan Halverson <daniel.m.halverson@gmail.com>
2015-08-23 20:52:06 -0500

    update to stock quote JS

--- HalversonWeb/WebContent/js/stock-quote.js
+++ HalversonWeb/WebContent/js/stock-quote.js
@@ -4,16 +4,15 @@ var stockQuote = angular.module('stockQuote', []);
 stockQuote.service('StockQuoteRetrieval', ['$http', function ($http) {
     this.call = function (stockSymbol) {
         var url = "http://dev.markitondemand.com/Api/v2/Quote/jsonp?jsoncallback=JSON_CALLBACK&symbol=" + stockSymbol;
-        var promise = $http.jsonp(url).then(function (response) {
+        return $http.jsonp(url).then(function (response) {
             console.info(response.data);
             return response.data;
         }, function (response) {
             var message = {};
-            message.Message = "The Stock Quote service could not retrieve your stock information at this time. Please try again later."
+            message.Message = "The Stock Quote service could not retrieve your stock information at this time. Please try again later.";
             return message;
         });
-        return promise;
-    }
+    };
 }]);
 stockQuote.controller('stockQuoteController', ['$scope', 'StockQuoteRetrieval', function ($scope, StockQuoteRetrieval) {
 
@@ -22,19 +21,23 @@ stockQuote.controller('stockQuoteController', ['$scope', 'StockQuoteRetrieval', 
     $scope.stockInput = "";
     var date = new Date();
     $scope.year = date.getFullYear();
-    for (var i = 0; i < $scope.stockList.length; i++) {
-        StockQuoteRetrieval.call($scope.stockList[i]).then(function (data) {
+    var makeCall = function(stockSymbol) {
+        StockQuoteRetrieval.call(stockSymbol).then(function (data) {
             $scope.stocks.push(data);
         });
+    };
+
+    for (var i = 0; i < $scope.stockList.length; i++) {
+        makeCall($scope.stockList[i]);
     }
 
     $scope.submit = function () {
         $scope.stockList.push($scope.stockInput);
-        StockQuoteRetrieval.call($scope.stockInput).then(function (data) {
-            $scope.stocks.push(data);
-        });
+        makeCall($scope.stockInput);
         $scope.stockInput = "";
     };
+
+
 }]);
 
 
